d1955e13dd5cf52b3b961cc2f2cec992f5acf11d
Nahom <nahomt@amestsantim.com>
2026-04-29 15:13:31 +0300

    Add base path to config to fix missing css in gh pages deploy

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Dashboard Dev't Course",
   description: "Dashboards for large-scale field data collection (Censuses and Surveys).",
+  base: '/dashboard-training/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
